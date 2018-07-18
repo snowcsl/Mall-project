fdfd003d257368d64d02ffdef626448d49616de7
xwp <xwp_fullstack@163.com>
2018-07-18 21:32:52 +0800

    qq登陆模型类

---
+++ meiduo_mall/meiduo_mall/apps/oauth/models.py
@@ -0,0 +1,17 @@
+# 定义QQ身份（openid）与用户模型类User的关联关系
+from django.db import models
+from meiduo_mall.utils.models import BaseModel
+
+
+# Create your models here.
+class OAuthQQUser(BaseModel):
+    """
+    QQ登录用户数据
+    """
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='用户')
+    openid = models.CharField(max_length=64, verbose_name='openid', db_index=True)
+
+    class Meta:
+        db_table = 'tb_oauth_qq'
+        verbose_name = 'QQ登录用户数据'
+        verbose_name_plural = verbose_name
