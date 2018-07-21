b090ca199f6504f9edff274ed2d9e39ddc408719
xwp <xwp_fullstack@163.com>
2018-07-21 13:38:09 +0800

    qq登录第三补

--- meiduo_mall/meiduo_mall/apps/oauth/utils.py
+++ meiduo_mall/meiduo_mall/apps/oauth/utils.py
@@ -1,7 +1,7 @@
-from urllib.parse import urlencode
-
+from urllib.parse import urlencode, parse_qs
+import requests
 from django.conf import settings
-
+import json
 
 class QAuthQQ(object):
     """
@@ -26,3 +26,81 @@ class QAuthQQ(object):
         qq_url = 'https://graph.qq.com/oauth2.0/authorize?' + urlencode(data_dict)  # 将data_dict字典转换为url路径中的查询字符串
 
         return qq_url
+
+    def get_access_token(self,code):
+        # 构建参数数据
+        data_dict = {
+            'grant_type':'authorization_code',
+            'client_id':self.client_id,
+            'client_secret':self.client_secret,
+            'redirect_uri':self.redirect_uri,
+            'code':code
+        }
+
+        # 构建url
+        # PC网站：https://graph.qq.com/oauth2.0/token
+        access_url = 'https://graph.qq.com/oauth2.0/token?'+urlencode(data_dict)
+
+        try:
+            # 发送请求
+            response = requests.get(access_url)
+
+            # 提取数据
+            # access_token=FE04************************CCE2&expires_in=7776000&refresh_token=88E4************************BE14
+            data = response.text # 返回查询字符串的形式
+
+            # 转化为字典
+            data = parse_qs(data)  # 将data查询字符串格式数据转换为python的字典
+        except:
+            raise Exception('qq请求失败')
+
+        # 提取access_token
+        access_token = data.get('access_token',None)
+
+        if not access_token:
+            raise Exception('access_token获取失败')
+
+        return access_token[0]
+
+    def get_open_id(self,access_token):
+        # 构建url
+        # PC网站：https://graph.qq.com/oauth2.0/me  需携带的参数:access_token
+        url= 'https://graph.qq.com/oauth2.0/me?access_token=' + access_token
+
+        try:
+
+            # 发送请求
+            response = requests.get(url)
+
+            # 提取数据
+            # callback( {"client_id":"YOUR_APPID","openid":"YOUR_OPENID"} );
+            # code=asdasd&msg=asjdhui  错误的时候返回的结果
+            data = response.text
+
+            data = data[10:-3]   # 通过切片切除字典形式的字符串
+        except:
+            raise Exception('qq请求失败')
+        try:
+
+            # 转化为字典
+            data_dict = json.loads(data)
+            # 获取openid
+            openid = data_dict.get('openid')
+        except:
+            raise Exception('openid获取失败')
+        return openid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,14 +1,17 @@
 from django.conf import settings
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_jwt.settings import api_settings
+from itsdangerous import TimedJSONWebSignatureSerializer as TJS
+from oauth.models import OAuthQQUser
+from oauth.serializers import OAuthQQUserSerializer
 from oauth.utils import QAuthQQ
 
 
 # Request URL:http://api.meiduo.site:8000/oauth/qq/authorization/?state=/
 class QQAuthURLView(APIView):
-
     def get(self, request):
-
         # 获取state状态码
         state = request.query_params.get('state', settings.QQ_STATE)
 
@@ -20,3 +23,75 @@ class QQAuthURLView(APIView):
 
         # 返回url
         return Response({'login_url': qq_url})
+
+
+# http://www.meiduo.site:8080/oauth_callback.html?code=F1169431F7654DFA954FC7689F1231A7&state=%2F
+class QQAuthUserView(APIView):
+    """
+    登陆QQ用户
+    """
+
+    def get(self, request):
+        # 获取code参数
+        code = request.query_params.get('code', None)
+
+        if not code:
+            return Response({'message': '缺少code值'}, status=status.HTTP_400_BAD_REQUEST)
+
+        qq = QAuthQQ()
+
+        try:
+            # 获取access_token
+            access_token = qq.get_access_token(code)
+
+            # 获取openid
+            openid = qq.get_open_id(access_token)
+
+        except Exception as e:
+            print(e)
+            return Response({'message': 'qq服务器异常'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 判断是否绑定通过
+        qq_user = OAuthQQUser.objects.get(openid=openid)
+
+        if not qq_user:
+
+            # 用户未绑定返回加密后的openid
+
+            tjs = TJS(settings.SECRET_KEY, 300)
+            data = {'openid': openid}
+
+            token = tjs.dumps(data).decode()
+
+            return Response({'access_token': token})
+
+        else:
+            # 用户已经绑定 获取用户对象
+
+            user = qq_user.user
+            # 写入JWT
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+            payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(payload)
+
+            return Response(
+                {
+                    'token': token,
+                    'user_id': user.id,
+                    'username': user.username
+                }
+            )
+
+    def post(self, request):
+        # 数据验证
+        ser = OAuthQQUserSerializer(data=request.data)
+        ser.is_valid()
+        print(ser.errors)
+
+        # 保存
+        ser.save()
+
+        # 返回数据
+        return Response(ser.data)
