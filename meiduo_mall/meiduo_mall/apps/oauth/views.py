74183a2a65f10464874be10888c51257e5038b7e
xwp <xwp_fullstack@163.com>
2018-07-21 13:48:49 +0800

    尝试失败

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,21 +1,30 @@
 from django.conf import settings
-from rest_framework import status
-from rest_framework.response import Response
+from django.shortcuts import render
 from rest_framework.views import APIView
+from urllib.parse import urlencode
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_jwt.settings import api_settings
 from itsdangerous import TimedJSONWebSignatureSerializer as TJS
+
 from oauth.models import OAuthQQUser
 from oauth.serializers import OAuthQQUserSerializer
 from oauth.utils import QAuthQQ
 
 
-# Request URL:http://api.meiduo.site:8000/oauth/qq/authorization/?state=/
+# Create your views here.
+
+
 class QQAuthURLView(APIView):
+    """
+    获取QQ登录的url
+    """
+
     def get(self, request):
-        # 获取state状态码
+        # 获取state
         state = request.query_params.get('state', settings.QQ_STATE)
 
-        # 创建QQ对象
+        # 创建qq对象
         qq = QAuthQQ(state=state)
 
         # 获取登录跳转的url
@@ -25,10 +34,10 @@ class QQAuthURLView(APIView):
         return Response({'login_url': qq_url})
 
 
-# http://www.meiduo.site:8080/oauth_callback.html?code=F1169431F7654DFA954FC7689F1231A7&state=%2F
+# http://api.meiduo.site:8000/oauth/qq/user/?code=DED76AB1331A9B83F509D919D6826DCF
 class QQAuthUserView(APIView):
     """
-    登陆QQ用户
+    QQ登录的用户
     """
 
     def get(self, request):
@@ -39,37 +48,34 @@ class QQAuthUserView(APIView):
             return Response({'message': '缺少code值'}, status=status.HTTP_400_BAD_REQUEST)
 
         qq = QAuthQQ()
-
         try:
-            # 获取access_token
+            # 获取access_token值
             access_token = qq.get_access_token(code)
-
-            # 获取openid
+            # 获取open_id值
             openid = qq.get_open_id(access_token)
-
         except Exception as e:
             print(e)
             return Response({'message': 'qq服务器异常'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # 判断是否绑定通过
-        qq_user = OAuthQQUser.objects.get(openid=openid)
+        # 判断是否绑定过openid
+        try:
+            qq_user = OAuthQQUser.objects.get(openid=openid)
+        except:
+            qq_user = None
 
         if not qq_user:
-
-            # 用户未绑定返回加密后的openid
-
+            # 用户未绑定
+            # 返回加密后的openid数据
             tjs = TJS(settings.SECRET_KEY, 300)
             data = {'openid': openid}
-
             token = tjs.dumps(data).decode()
-
             return Response({'access_token': token})
 
         else:
-            # 用户已经绑定 获取用户对象
-
+            # 用户已绑定
+            # 获取用户对象
             user = qq_user.user
-            # 写入JWT
+            # 写入jwt
             jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
             jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
