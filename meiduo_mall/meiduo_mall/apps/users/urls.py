6bb65a4c363797683c17a39fefed42b1ce961a57
xwp <xwp_fullstack@163.com>
2018-07-21 13:47:25 +0800

    尝试

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^usernames/(?P<username>\w{5,20})/count/$', views.UsernameCountView.as_view()),
     url(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
     url(r'^users/$', views.UserView.as_view()),
-    url(r'^authorizations/$', obtain_jwt_token),  # Django REST framework JWT提供了登录签发JWT的视图，可以直接使用
-
+    url(r'^authorizations/$', obtain_jwt_token),  # Django REST framework JWT提供了登录签发JWT的视图，可以直接使用 完成认证过程以及写入token的过程
+    #  obtain_jwt_token 结果返回只会返回token值,不显示用户名,原生的函数不能满足要求,重写
 ]
--- meiduo_mall/meiduo_mall/apps/users/utils.py
+++ meiduo_mall/meiduo_mall/apps/users/utils.py
@@ -1,30 +1,34 @@
 import re
 
 from django.contrib.auth.backends import ModelBackend
+# django原生的认证类,在类下面进行重写,因为原生中的用户名只支持字符串,为了使用户名既可以是手机号  也可以是字符串形式
 
 from users.models import User
 
 
 def jwt_response_payload_handler(token, user=None, request=None):
+    # 只需要接收处理好的数据,只用于返回  在配置文件中可以找到此视图即可
     """
     自定义jwt认证成功返回数据
     """
     return {
-        'token': token,
+        'token': token,  # 已经生成好的token
         'user_id': user.id,
         'username': user.username
     }
 
 
-def get_user_by_account(account):
+def get_user_by_account(username):
     """根据账号获取用户对象"""
     try:
-        if re.match(r'^1[3-9]\d{9}$', account):
+        # 按手机号查询
+        if re.match(r'^1[3-9]\d{9}$', username):
             # 手机号
-            user = User.objects.get(mobile=account)
+            user = User.objects.get(mobile=username)
         else:
-            user = User.objects.get(username=account)
-    except User.DoesNotExist:
+            # 按用户名查询
+            user = User.objects.get(username=username)
+    except User.DoesNotExist: # 一旦捕获到异常,说明用户不存在
         return None
     else:
         return user
@@ -36,12 +40,11 @@ class UsernameMobileAuthBackend(ModelBackend):
     :param ModelBackend:
     :return:
     """
-    def authenticate(self, request, username=None, password=None, **kwargs):
+
+    def authenticate(self, request, username=None, password=None, **kwargs):  # username 接收相应框内的数据  不仅仅只带用户名
         """username可以是用户名也可以是手机号"""
         # 获取用户对象
         user = get_user_by_account(username)
-
-        if user is not None and user.check_password(password):
+        # 比对校验
+        if user is not None and user.check_password(password):  # 进行密码校验
             return user
-
-
