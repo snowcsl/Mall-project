8bc43aece4c4d02470979e52d2a98c9ad96a3515
xwp <xwp_fullstack@163.com>
2018-07-22 20:41:05 +0800

    标注信息更新

--- meiduo_mall/meiduo_mall/apps/areas/models.py
+++ meiduo_mall/meiduo_mall/apps/areas/models.py
@@ -9,9 +9,10 @@ class Area(models.Model):
     name = models.CharField(max_length=20, verbose_name='名称')
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='subs', null=True, blank=True,
                                verbose_name='上级行政区划')
-
+    # SET_NULL  删除省信息，下级信息置为空
     # 自关联字段的外键指向自身 ForeignKey('self')
-    # related_name指明查询一个行政区划的所有下级行政区划时，使用哪种语法查询   Area模型类对象.subs查询
+    # related_name指明查询一个行政区划的所有下级行政区划时，使用哪种语法查询   主表查询附表时 由于自关联  Area模型类对象.subs查询
+    # blank=True 数据查询可以不传省的数据
 
     class Meta:
         db_table = 'tb_areas'
--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -53,15 +53,19 @@ class Address(BaseModel):
     """
     用户地址
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    # 根据前端传递传来的数据地址来定义
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses',
+                             verbose_name='用户')  # 当前地址属于哪个用户  通过用户外外键关联
     title = models.CharField(max_length=20, verbose_name='地址名称')
     receiver = models.CharField(max_length=20, verbose_name='收货人')
+
     province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses',
                                  verbose_name='省')
     city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='市')
     district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses',
-                                 verbose_name='区')
-    place = models.CharField(max_length=50, verbose_name='地址')
+                                 verbose_name='区')  # ForeignKey通过id值进行保存
+
+    place = models.CharField(max_length=50, verbose_name='地址')  # 详情地址
     mobile = models.CharField(max_length=11, verbose_name='手机')
     tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
     email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='电子邮箱')
