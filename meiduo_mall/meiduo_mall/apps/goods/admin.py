c71d9bff5d21b29c2ed5635a6cfc7135844972c8
xwp <xwp_fullstack@163.com>
2018-07-25 21:59:50 +0800

    异步任务的触发

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -11,13 +11,50 @@ class SKUAdmin(admin.ModelAdmin):
         generate_static_sku_detail_html.delay(obj.id)
         # 注意启动异步任务 celery -A celery_tasks.main worker -l info
 
+    def delete_model(self, request, obj):
+        pass
+
+
+class SpecificationOptionAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        sku_id = obj.sku.id
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
+    def delete_model(self, request, obj):
+        sku_id = obj.sku.id
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
+
+class SKUImageAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        sku_id = obj.sku.id
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
+        # 设置SKU默认图片
+        sku = obj.sku
+        if not sku.default_image_url:
+            sku.default_image_url = obj.image.url
+            sku.save()
+
+    def delete_model(self, request, obj):
+        sku_id = obj.sku.id
+        obj.delete()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(sku_id)
+
 
 admin.site.register(models.GoodsCategory)
 admin.site.register(models.GoodsChannel)
 admin.site.register(models.Goods)
 admin.site.register(models.Brand)
 admin.site.register(models.GoodsSpecification)
-admin.site.register(models.SpecificationOption)
-admin.site.register(models.SKU,SKUAdmin)
+admin.site.register(models.SpecificationOption, SpecificationOptionAdmin)
+admin.site.register(models.SKU, SKUAdmin)
 admin.site.register(models.SKUSpecification)
-admin.site.register(models.SKUImage)
+admin.site.register(models.SKUImage, SKUImageAdmin)
