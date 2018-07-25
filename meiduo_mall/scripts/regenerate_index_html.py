658a8defaf68c55931e7960dffda7bddfc178dff
xwp <xwp_fullstack@163.com>
2018-07-25 21:15:28 +0800

    静态化首页的手动脚本

---
+++ meiduo_mall/scripts/regenerate_index_html.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+功能：手动生成所有SKU的静态detail html文件
+使用方法:
+    ./regenerate_index_html.py
+"""
+
+# 指定django中的导包路径
+import sys
+sys.path.insert(0, '../')
+
+# 导入django的配置文件，因为需要调用到django
+import os
+if not os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'meiduo_mall.settings.dev'
+
+# 让django进行初始化设置，相当于进入shell交互环境
+import django
+django.setup()
+
+from contents.crons import generate_static_index_html
+
+if __name__ == '__main__':
+    generate_static_index_html()
