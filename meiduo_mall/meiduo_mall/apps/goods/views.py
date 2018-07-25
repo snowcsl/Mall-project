164435e7cc25cdefd338d53a3c89ae935b1c97ab
xwp <xwp_fullstack@163.com>
2018-07-25 23:04:11 +0800

    热销商品方式纠正及第二种方法

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView,GenericAPIView
+from rest_framework.response import Response
 from rest_framework_extensions.cache.mixins import ListCacheResponseMixin
 from goods.models import SKU
 
 # Create your views here.
 
 from goods.serializers import SKUSerializer
+
+
+# class HotSKUListView(GenericAPIView):
+#     """
+#     热销商品, 使用缓存扩展
+#     """
+#     serializer_class = SKUSerializer
+#
+#     def get(self, request, category_id):
+#         hot_skus = SKU.objects.filter(category_id=category_id, is_launched=True).order_by('-sales')[:5]
+#         ser = self.get_serializer(hot_skus, many=True)
+#         return Response(ser.data)
 
 
 class HotSKUListView(ListAPIView):
@@ -15,7 +28,8 @@ class HotSKUListView(ListAPIView):
     serializer_class = SKUSerializer
 
     def get_queryset(self):
-        category_id=self.kwargs['category_id ']
-        hot_sku=SKU.objects.filter(category_id=category_id,is_launched=True).order_by('-sale')[:5]
-        return hot_sku
+
+        category_id=self.kwargs['category_id']
+        return SKU.objects.filter(category_id=category_id,is_launched=True).order_by('-sales')[:5]
+
 
