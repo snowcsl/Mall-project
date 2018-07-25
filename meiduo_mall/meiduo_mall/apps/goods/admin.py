b955ff8b64dc05d1045153d185e22439c47d3367
xwp <xwp_fullstack@163.com>
2018-07-25 21:44:10 +0800

    商品详情页--静态化处理 异步任务的应用

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from goods import models
+
+
 # Register your models here.
+
+class SKUAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        from celery_tasks.html.tasks import generate_static_sku_detail_html
+        generate_static_sku_detail_html.delay(obj.id)
+        # 注意启动异步任务 celery -A celery_tasks.main worker -l info
 
 
 admin.site.register(models.GoodsCategory)
@@ -9,6 +18,6 @@ admin.site.register(models.Goods)
 admin.site.register(models.Brand)
 admin.site.register(models.GoodsSpecification)
 admin.site.register(models.SpecificationOption)
-admin.site.register(models.SKU)
+admin.site.register(models.SKU,SKUAdmin)
 admin.site.register(models.SKUSpecification)
 admin.site.register(models.SKUImage)
