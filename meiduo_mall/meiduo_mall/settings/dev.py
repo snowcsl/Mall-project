95e1554b2c9f257c3f748d3a3057cbcffd543b5a
xwp <xwp_fullstack@163.com>
2018-07-25 10:41:40 +0800

    自定义Django文件存储系统

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -51,7 +51,10 @@ INSTALLED_APPS = [
     'user_center.apps.UserCenterConfig',
     'areas.apps.AreasConfig',
     'goods.apps.GoodsConfig',
-    'contents.apps.ContentsConfig'
+    'contents.apps.ContentsConfig',
+    'ckeditor',  # 富文本编辑器
+    'ckeditor_uploader',  # 富文本编辑器上传图片模块
+
 
 ]
 
@@ -272,3 +275,20 @@ EMAIL_FROM = 'python<18800265204@163.com>'
 #     # 缓存存储
 #     'DEFAULT_USE_CACHE': 'default',
 # }
+
+# django文件存储
+DEFAULT_FILE_STORAGE = 'meiduo_mall.utils.fastdfs.fdfs_storage.FastDFSStorage'
+
+# FastDFS
+FDFS_URL = 'http://image.meiduo.site:8888/'
+FDFS_CLIENT_CONF = os.path.join(BASE_DIR, 'utils/fastdfs/client.conf')
+
+# 富文本编辑器ckeditor配置
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',  # 工具条功能
+        'height': 300,  # 编辑器高度
+        # 'width': 300,  # 编辑器宽
+    },
+}
+CKEDITOR_UPLOAD_PATH = ''  # 上传图片保存路径，使用了FastDFS，所以此处设为''
